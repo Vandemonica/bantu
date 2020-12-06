abbbe752c20d22e43eeed140cfaad632458b88ce
rahman <ainurahman03@gmail.com>
2020-12-06 10:41:14 +0700

    menjadikan navbar menjadi sticky top agar tidak hilang saat berpindah ke gambar yang jauh dibawah

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@ function show(id) {
     let selct_id = document.getElementById(id);
     selct_id.style.visibility = "visible";
     location.href = "#" + id
+    if (location.href == "None") {
+        location.href = "#home"
+    }
 }
 
 function sembunyi(id) {
